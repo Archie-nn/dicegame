d967c89ae9989ebb39291813c4cc40bcc69e92d8
user <user@1008-063>
2026-05-25 17:31:51 +0800

    Add project files.

---
+++ dicegame/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace dicegame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.numBet = new System.Windows.Forms.NumericUpDown();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblMoney = new System.Windows.Forms.Label();
+            this.picP1 = new System.Windows.Forms.PictureBox();
+            this.picP2 = new System.Windows.Forms.PictureBox();
+            this.picP3 = new System.Windows.Forms.PictureBox();
+            this.picP4 = new System.Windows.Forms.PictureBox();
+            this.picC1 = new System.Windows.Forms.PictureBox();
+            this.picC2 = new System.Windows.Forms.PictureBox();
+            this.picC3 = new System.Windows.Forms.PictureBox();
+            this.picC4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numBet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(236, 371);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(75, 23);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "擲骰";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // numBet
+            // 
+            this.numBet.Location = new System.Drawing.Point(515, 371);
+            this.numBet.Name = "numBet";
+            this.numBet.Size = new System.Drawing.Size(120, 22);
+            this.numBet.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(351, 297);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 6;
+            // 
+            // lblMoney
+            // 
+            this.lblMoney.AutoSize = true;
+            this.lblMoney.Location = new System.Drawing.Point(379, 376);
+            this.lblMoney.Name = "lblMoney";
+            this.lblMoney.Size = new System.Drawing.Size(33, 12);
+            this.lblMoney.TabIndex = 7;
+            this.lblMoney.Text = "label6";
+            this.lblMoney.Click += new System.EventHandler(this.lblMoney_Click);
+            // 
+            // picP1
+            // 
+            this.picP1.Location = new System.Drawing.Point(666, 116);
+            this.picP1.Name = "picP1";
+            this.picP1.Size = new System.Drawing.Size(100, 50);
+            this.picP1.TabIndex = 8;
+            this.picP1.TabStop = false;
+            // 
+            // picP2
+            // 
+            this.picP2.Location = new System.Drawing.Point(524, 116);
+            this.picP2.Name = "picP2";
+            this.picP2.Size = new System.Drawing.Size(100, 50);
+            this.picP2.TabIndex = 9;
+            this.picP2.TabStop = false;
+            // 
+            // picP3
+            // 
+            this.picP3.Location = new System.Drawing.Point(524, 222);
+            this.picP3.Name = "picP3";
+            this.picP3.Size = new System.Drawing.Size(100, 50);
+            this.picP3.TabIndex = 10;
+            this.picP3.TabStop = false;
+            // 
+            // picP4
+            // 
+            this.picP4.Location = new System.Drawing.Point(666, 222);
+            this.picP4.Name = "picP4";
+            this.picP4.Size = new System.Drawing.Size(100, 50);
+            this.picP4.TabIndex = 11;
+            this.picP4.TabStop = false;
+            // 
+            // picC1
+            // 
+            this.picC1.Location = new System.Drawing.Point(12, 116);
+            this.picC1.Name = "picC1";
+            this.picC1.Size = new System.Drawing.Size(100, 50);
+            this.picC1.TabIndex = 12;
+            this.picC1.TabStop = false;
+            // 
+            // picC2
+            // 
+            this.picC2.Location = new System.Drawing.Point(128, 116);
+            this.picC2.Name = "picC2";
+            this.picC2.Size = new System.Drawing.Size(100, 50);
+            this.picC2.TabIndex = 13;
+            this.picC2.TabStop = false;
+            // 
+            // picC3
+            // 
+            this.picC3.Location = new System.Drawing.Point(12, 222);
+            this.picC3.Name = "picC3";
+            this.picC3.Size = new System.Drawing.Size(100, 50);
+            this.picC3.TabIndex = 14;
+            this.picC3.TabStop = false;
+            // 
+            // picC4
+            // 
+            this.picC4.Location = new System.Drawing.Point(128, 222);
+            this.picC4.Name = "picC4";
+            this.picC4.Size = new System.Drawing.Size(100, 50);
+            this.picC4.TabIndex = 15;
+            this.picC4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picC4);
+            this.Controls.Add(this.picC3);
+            this.Controls.Add(this.picC2);
+            this.Controls.Add(this.picC1);
+            this.Controls.Add(this.picP4);
+            this.Controls.Add(this.picP3);
+            this.Controls.Add(this.picP2);
+            this.Controls.Add(this.picP1);
+            this.Controls.Add(this.lblMoney);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.numBet);
+            this.Controls.Add(this.btnRoll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numBet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picP4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.NumericUpDown numBet;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblMoney;
+        private System.Windows.Forms.PictureBox picP1;
+        private System.Windows.Forms.PictureBox picP2;
+        private System.Windows.Forms.PictureBox picP3;
+        private System.Windows.Forms.PictureBox picP4;
+        private System.Windows.Forms.PictureBox picC1;
+        private System.Windows.Forms.PictureBox picC2;
+        private System.Windows.Forms.PictureBox picC3;
+        private System.Windows.Forms.PictureBox picC4;
+    }
+}
+
---
+++ dicegame/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace dicegame
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+
+        int playerMoney = 1000;
+        int betAmount = 0;
+
+        int playerScore = 0;
+        int computerScore = 0;
+
+        bool playerLeopard = false;
+        bool computerLeopard = false;
+
+        PictureBox[] playerPics;
+        PictureBox[] computerPics;
+
+        Image[] diceImages;
+
+        SoundPlayer rollSound,checkSound,enoughSound,playerSound,pcSound,tieSound;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateMoney();
+            playerPics = new PictureBox[]
+            {
+                  picP1,picP2,picP3,picP4
+            };
+
+            computerPics = new PictureBox[]
+            {
+                  picC1,picC2,picC3,picC4
+            };
+
+            // 載入骰子圖片
+            diceImages = new Image[]
+            {
+                 Image.FromFile("Images/dice1.png"),
+                 Image.FromFile("Images/dice2.png"),
+                 Image.FromFile("Images/dice3.png"),
+                 Image.FromFile("Images/dice4.png"),
+                 Image.FromFile("Images/dice5.png"),
+                 Image.FromFile("Images/dice6.png")
+            };
+
+            rollSound = new SoundPlayer("Sounds/roll.wav");
+            checkSound = new SoundPlayer("Sounds/check.wav");
+            enoughSound = new SoundPlayer("Sounds/enough.wav");
+            playerSound = new SoundPlayer("Sounds/playerwin.wav");
+            pcSound = new SoundPlayer("Sounds/pcwin.wav");
+            tieSound = new SoundPlayer("Sounds/tie.wav");
+
+        }
+        private async Task AnimateDice(PictureBox[] pics)
+        {
+            rollSound.Play();
+
+            // 記錄原始位置與大小
+            Point[] originalLocations = new Point[pics.Length];
+            Size[] originalSizes = new Size[pics.Length];
+
+            for (int i = 0; i < pics.Length; i++)
+            {
+                originalLocations[i] = pics[i].Location;
+                originalSizes[i] = pics[i].Size;
+            }
+
+            // 動畫循環
+            for (int t = 0; t < 20; t++)
+            {
+                for (int i = 0; i < pics.Length; i++)
+                {
+                    int value = rand.Next(1, 7);
+
+                    pics[i].Image = diceImages[value - 1];
+
+                    // ===== 抖動效果 =====
+
+                    int offsetX = rand.Next(-8, 9);
+                    int offsetY = rand.Next(-8, 9);
+
+                    pics[i].Location = new Point(
+                        originalLocations[i].X + offsetX,
+                        originalLocations[i].Y + offsetY
+                    );
+
+                    // ===== 縮放效果 =====
+
+                    int scale = rand.Next(-10, 11);
+
+                    pics[i].Size = new Size(
+                        originalSizes[i].Width + scale,
+                        originalSizes[i].Height + scale
+                    );
+
+                    pics[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+
+                await Task.Delay(50);
+            }
+
+            // 還原位置與大小
+            for (int i = 0; i < pics.Length; i++)
+            {
+                pics[i].Location = originalLocations[i];
+                pics[i].Size = originalSizes[i];
+            }
+        }
+        private async Task<int[]> RollDiceWithAnimation(PictureBox[] pics)
+        {
+            await AnimateDice(pics);
+
+            int[] dice;
+
+            while (true)
+            {
+                dice = new int[4];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    dice[i] = rand.Next(1, 7);
+                }
+
+                if (HasPair(dice))
+                    break;
+            }
+
+            // 顯示最終骰面
+            for (int i = 0; i < 4; i++)
+            {
+                pics[i].Image = diceImages[dice[i] - 1];
+            }
+            // 停止時放大一下
+            for (int i = 0; i < pics.Length; i++)
+            {
+                pics[i].Size = new Size(110, 110);
+            }
+
+            await Task.Delay(100);
+
+            for (int i = 0; i < pics.Length; i++)
+            {
+                pics[i].Size = new Size(100, 100);
+            }
+            return dice;
+        }
+
+        private async void btnRoll_Click(object sender, EventArgs e)
+        {
+            btnRoll.Enabled = false;
+
+            betAmount = (int)numBet.Value;
+
+            if (betAmount <= 0)
+            {
+
+                checkSound.Play(); 
+                MessageBox.Show("請下注");
+                btnRoll.Enabled = true;
+                return;
+            }
+
+            if (betAmount > playerMoney)
+            {
+                enoughSound.Play();
+                MessageBox.Show("資金不足");
+                btnRoll.Enabled = true;
+                return;
+            }
+
+            playerMoney -= betAmount;
+
+            // 玩家擲骰動畫
+            int[] playerDice =
+                await RollDiceWithAnimation(playerPics);
+
+            playerScore =
+                CalculateScore(playerDice, out playerLeopard);
+
+            // 電腦擲骰動畫
+            await Task.Delay(500);
+
+            int[] computerDice =
+                await RollDiceWithAnimation(computerPics);
+
+            computerScore =
+                CalculateScore(computerDice, out computerLeopard);
+
+            JudgeWinner();
+
+            UpdateMoney();
+
+            btnRoll.Enabled = true;
+        }
+
+        // =========================
+        // 是否存在對子
+        // =========================
+        private bool HasPair(int[] dice)
+        {
+            var groups = dice.GroupBy(x => x);
+
+            return groups.Any(g => g.Count() >= 2);
+        }
+        // =========================
+        // 計算分數
+        // =========================
+        private int CalculateScore(int[] dice, out bool leopard)
+        {
+            leopard = false;
+
+            var groups = dice.GroupBy(x => x)
+                             .OrderByDescending(g => g.Count())
+                             .ThenByDescending(g => g.Key)
+                             .ToList();
+
+            // 豹子：四顆相同
+            if (groups[0].Count() == 4)
+            {
+                leopard = true;
+                return 100; // 豹子最高分
+            }
+
+            // 三顆相同 + 單張
+            if (groups[0].Count() == 3)
+            {
+                return groups[1].Key;
+            }
+
+            // 兩組對子
+            if (groups.Count == 2 &&
+                groups[0].Count() == 2 &&
+                groups[1].Count() == 2)
+            {
+                return Math.Max(groups[0].Key, groups[1].Key);
+            }
+
+            // 一組對子
+            if (groups[0].Count() == 2)
+            {
+                int sum = groups.Where(g => g.Count() == 1)
+                                .Sum(g => g.Key);
+
+                return sum;
+            }
+
+            return 0;
+        }
+
+        // =========================
+        // 判定勝負
+        // =========================
+        private void JudgeWinner()
+        {
+            // 豹子優先
+            if (playerLeopard && !computerLeopard)
+            {
+                int reward = betAmount * 3; // 本金 + 2倍獎金
+
+                playerMoney += reward;
+
+                lblResult.Text =
+                    $"玩家豹子！獲得 {reward} 元";
+            }
+            else if (!playerLeopard && computerLeopard)
+            {
+                lblResult.Text = "電腦豹子，玩家失敗";
+            }
+            else
+            {
+                if (playerScore > computerScore)
+                {
+                    int reward = betAmount; // 本金 + 1倍獎金
+
+                    playerMoney += reward*2;
+
+                    lblResult.Text =
+                        $"玩家勝利！獲得 {reward} 元";
+                    playerSound.Play();
+                }
+                else if (playerScore < computerScore)
+                {
+                    lblResult.Text = "電腦勝利";
+                    pcSound.Play();
+                }
+                else
+                {
+                    // 平手退回本金
+                    playerMoney += betAmount;
+
+                    lblResult.Text = "平手，退回本金";
+                    tieSound.Play();
+                }
+            }
+        }
+
+        // =========================
+        // 更新資金
+        // =========================
+        private void UpdateMoney()
+        {
+            lblMoney.Text = $"目前資金：{playerMoney}";
+        }
+
+        private void lblMoney_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
